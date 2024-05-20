1d88cd90040292aa090d1f8ac0ad65e84667f703
Fabricio <casta2804@gmail.com>
2024-05-19 23:55:07 -0600

    profile1

--- lib/pages/profile/profile_controller.dart
+++ lib/pages/profile/profile_controller.dart
@@ -1,3 +1,24 @@
 import 'package:get/get.dart';
+import 'package:ulimagym/models/entities/Usuario.dart';
 
-class ProfileController extends GetxController {}
+class ProfileController extends GetxController {
+  var usuario = Usuario.empty().obs;
+
+  @override
+  void onInit() {
+    // Aquí puedes cargar los datos del usuario
+    super.onInit();
+  }
+
+  void cargarUsuario(Usuario user) {
+    usuario.value = user;
+  }
+
+  void cambiarContrasenia() {
+    // Lógica para cambiar contraseña
+  }
+
+  void cerrarSesion() {
+    // Lógica para cerrar sesión
+  }
+}
