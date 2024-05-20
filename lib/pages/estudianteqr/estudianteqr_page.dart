5b9982618c4b842aa20f35f2acd4802e283d1046
Renzo <renzoar@hotmail.com>
2024-05-19 21:32:48 -0500

    Avance estudianteqr

--- lib/pages/estudianteqr/estudianteqr_page.dart
+++ lib/pages/estudianteqr/estudianteqr_page.dart
@@ -2,27 +2,66 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ulimagym/models/entities/Usuario.dart';
 import 'estudianteqr_controller.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-class EstudianteQRPage extends StatelessWidget {
+class EstudianteQRPage extends StatefulWidget {
   final Usuario usuario;
   EstudianteQRPage({required this.usuario});
+  @override
+  State<StatefulWidget> createState() => EstudianteQR();
+}
 
-  EstudianteQRController control = Get.put(EstudianteQRController());
+class EstudianteQR extends State<EstudianteQRPage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+  String? qrText;
 
-  Widget _buildBody(BuildContext context) {
-    return SafeArea(
-      child: Text('Leer QR'),
-    );
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (controller != null) {
+      controller!.pauseCamera();
+    }
   }
 
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: null,
-      body: _buildBody(context),
-    ));
+    return Scaffold(
+      appBar: AppBar(title: Text('Escanear QR')),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: (qrText != null)
+                  ? Text('Código QR: $qrText')
+                  : Text('Escanea un código QR'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        qrText = scanData.code;
+      });
+    });
   }
 }
