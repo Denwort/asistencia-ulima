25ef38e6e13559cdc6eb26e1cfa422a258bd12a3
Mateo <mateo27965@gmail.com>
2024-06-05 18:07:35 -0500

    profesor

--- lib/models/entities/Seccion.dart
+++ lib/models/entities/Seccion.dart
@@ -13,14 +13,14 @@ class Seccion {
   String codigo;
   Periodo periodo;
   Curso curso;
-  Usuario usuario;
+  Usuario profesor;
 
   Seccion({
     required this.id,
     required this.codigo,
     required this.periodo,
     required this.curso,
-    required this.usuario,
+    required this.profesor,
   });
 
   Seccion.empty()
@@ -28,14 +28,14 @@ class Seccion {
         codigo = '',
         periodo = Periodo.empty(),
         curso = Curso.empty(),
-        usuario = Usuario.empty();
+        profesor = Usuario.empty();
 
   factory Seccion.fromJson(Map<String, dynamic> json) => Seccion(
         id: json["id"],
         codigo: json["codigo"],
         periodo: Periodo.fromJson(json["periodo"]),
         curso: Curso.fromJson(json["curso"]),
-        usuario: Usuario.fromJson(json["usuario"]),
+        profesor: Usuario.fromJson(json["profesor"]),
       );
 
 // cambie el nombre de profesor a usuario
@@ -44,11 +44,11 @@ class Seccion {
         "codigo": codigo,
         "periodo": periodo.toJson(),
         "curso": curso.toJson(),
-        "usuario": usuario.toJson(),
+        "profesor": profesor.toJson(),
       };
   @override
   String toString() {
-    return 'Seccion -> id: ${id}, codigo: ${this.codigo}, periodo: ${this.periodo}, curso: ${this.curso}, usuario: ${this.usuario}';
+    return 'Seccion -> id: ${id}, codigo: ${this.codigo}, periodo: ${this.periodo}, curso: ${this.curso}, prfeosro: ${this.profesor}';
   }
 
   static List<Seccion> lista = [
@@ -58,21 +58,21 @@ class Seccion {
       codigo: '830',
       periodo: '2024-1',
       curso: 'Programacion movil',
-      usuario: Usuario.lista[0],
+      profesor: Usuario.lista[0],
     ),
     Seccion(
       id: 1,
       codigo: '833',
       periodo: '2024-1',
       curso: 'Taller de propuesta de investigacion',
-      usuario: Usuario.lista[0],
+      profesor: Usuario.lista[0],
     ),
     Seccion(
       id: 2,
       codigo: '831',
       periodo: '2024-1',
       curso: 'Ingenieria de software 2',
-      usuario: Usuario.lista[1],
+      profesor: Usuario.lista[1],
     )
   ];
 }
